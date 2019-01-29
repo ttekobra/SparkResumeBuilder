f2af9c7abe8a2af34bf9ca200685a9ff63ef9676
Suprem Nandal <Suprem.nandal@gmail.com>
2019-01-29 22:24:30 +0530

    Before ViewPager

---
+++ app/src/main/java/com/ttekobra/sparkresume/Fragments/Pager.java
@@ -0,0 +1,27 @@
+package com.ttekobra.sparkresume.Fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import java.util.List;
+
+public class Pager extends FragmentStatePagerAdapter {
+
+    private List<Fragment> fragmentList;
+
+    public Pager(FragmentManager fm, List<Fragment> fragmentList) {
+        super(fm);
+        this.fragmentList = fragmentList;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
